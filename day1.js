05435b2a0e0da31413e5b189152d3d3cf86c15e8
Bhavika Devani <devanibhavika2002@gmail.com>
2025-04-30 15:22:47 +0530

    Initial commit - Added portfolio HTML files

---
+++ day1.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Arithmetic operators</title>
+    </head>
+    <body>
+        <h1>Arithmetic operators</h1>
+        <script>
+            var a=10,b=20;
+            var c=a+b;
+            var d=a-b;
+            var e=a*b;
+            var f=a/b;
+            var g=a%b;
+            console.log(c,d,e,f,g);
+        </script>
+    </body>
+</html>
---
+++ javascript.js
@@ -0,0 +1,6 @@
+<script>
+    a=10;
+    b=5;
+    if(a>b)
+        console.log(a);
+</script>
